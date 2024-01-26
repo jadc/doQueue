5789cf71b2ec21b4fd743d04da55152de075272c
jadc <chehimj@gmail.com>
2024-01-25 22:34:08 -0700

    attempt at state

--- src/app/[room]/page.tsx
+++ src/app/[room]/page.tsx
@@ -1,7 +1,21 @@
-import { useRouter } from 'next/router';
+"use client"
 
-export default function Room({ params }) {
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react"
+
+export default function Room({ params }: any) {
+
+    const [owner, setOwner] = useState(false)
+
+    // Determine if client owns room
+    useEffect(() => {
+        setOwner( params.room === window.localStorage.getItem("id") )
+    }, [])
+
     return (
-        <h1>Room: {params.room}</h1>
+        <div>
+            <h1>Room: {params.room}</h1>
+            <p>Owner? {owner ? "Yes" : "No"}</p>
+        </div>
     );
 };
