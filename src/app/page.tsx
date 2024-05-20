0d4057bec04a6315be10dc3e31a4859407d85f9b
jadc <chehimj@gmail.com>
2024-05-20 15:58:35 -0600

    restart

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { redirect } from "next/navigation";
-
-export default function Home() {
-    // Generate unique identifier and room for new user
-    const clientRoom = (Math.random() + 1).toString(36).substring(2);
-    window.localStorage.setItem("id", clientRoom);
-    return (redirect(clientRoom));
-}
